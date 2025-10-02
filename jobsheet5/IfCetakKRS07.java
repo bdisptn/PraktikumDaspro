ff4f8c3bfba96d80dddf7db34d71c05835fc9fde
bdisptn <budisptn13@gmail.com>
2025-10-02 11:41:00 +0700

    Tugas 1

--- jobsheet5/IfCetakKRS07.java
+++ jobsheet5/IfCetakKRS07.java
@@ -5,12 +5,18 @@ public class IfCetakKRS07 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        boolean uktLunas;
-
+        //Input
         System.out.println("--- Cetak KRS SIAKAD ---");
         System.out.print("Apakah UKT sudah lunas? (true/false): ");
-        uktLunas = sc.nextBoolean();
+        boolean uktLunas = sc.nextBoolean();
 
+        //Ternary Operator
+        String pesan = (uktLunas) ? "Pembayaran UKT terverifikasi.\nSilakan cetak KRS dan minta tanda tangan DPA" : "Registrasi ditolak.\nSilakan lunasi UKT terlebih dahulu";
+
+        //Output pesan
+        System.out.println(pesan);
+
+        //IF-ELSE Operator
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
